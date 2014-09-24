a660407eac524c8cd63b4641c502b8e3230e5039
kavitam <kavita_mantripragada@hotmail.com>
2014-09-24 11:46:01 -0400

    Added latest change from preetigupta152 area - Added catch block that returns broken resource in case of Exception.

--- src/main/java/redis/clients/johm/Nest.java
+++ src/main/java/redis/clients/johm/Nest.java
@@ -113,357 +113,677 @@ public class Nest<T> {
     
     // Redis Common Operations
     public String set(String value) {
+    	Boolean ex = false;
     	Jedis jedis = null;
     	try{
     		jedis = getResource();
     		String set = jedis.set(key(), value);
     		return set;
-    	}finally {
-    		if (jedis != null) returnResource(jedis);
+    	}catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    }finally {
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public String get() {
+    	Boolean ex =false;
     	Jedis jedis = null;
     	try{
     		jedis = getResource();
     		String string = jedis.get(key());
     		return string;
-    	}finally {
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally {
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Long incr() {
+    	Boolean ex = false;
     	Jedis jedis = null;
     	try{
     		jedis = getResource();
     		Long incr = jedis.incr(key());
     		return incr;
-    	}finally {
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally {
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public List<Object> multi(TransactionBlock transaction) {
+    	Boolean ex =false;
     	Jedis jedis = null;
     	try{
     		jedis = getResource();
     		List<Object> multi = jedis.multi(transaction);
     		return multi;
-    	}finally {
-    		if (jedis != null) returnResource(jedis);
+    	}catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally {
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public List<Object> multiWithWatch(TransactionBlock transaction, String...keys) {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		jedis.watch(keys);
     		List<Object> multi = jedis.multi(transaction);
     		return multi;
-    	}finally {
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    }finally {
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Long del() {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		Long del = jedis.del(key());
     		return del;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Boolean exists() {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		Boolean exists = jedis.exists(key());
     		return exists;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	}catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     // Redis Hash Operations
     public String hmset(Map<String, String> hash) {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		String hmset = jedis.hmset(key(), hash);
     		return hmset;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Map<String, String> hgetAll() {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		Map<String, String> hgetAll = jedis.hgetAll(key());
     		return hgetAll;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public String hget(String field) {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		String value = jedis.hget(key(), field);
     		return value;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Long hdel(String field) {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		Long hdel = jedis.hdel(key(), field);
     		return hdel;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Long hlen() {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		Long hlen = jedis.hlen(key());
     		return hlen;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Set<String> hkeys() {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		Set<String> hkeys = jedis.hkeys(key());
     		return hkeys;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     // Redis Set Operations
     public Long sadd(String member) {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		Long reply = jedis.sadd(key(), member);
     		return reply;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Long srem(String member) {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		Long reply = jedis.srem(key(), member);
     		return reply;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Set<String> smembers() {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		Set<String> members = jedis.smembers(key());
     		return members;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Set<String> sinter() {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		Set<String> members = jedis.sinter((String[])keys.toArray(new String[0]));
     		return members;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public void sinterstore(final String dstkey) {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		jedis.sinterstore(dstkey, (String[])keys.toArray(new String[0]));
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex){
+    			returnResource(jedis);
+    		}
     	}
     }
 
     // Redis List Operations
     public Long rpush(String string) {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		Long rpush = jedis.rpush(key(), string);
     		return rpush;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public String lset(int index, String value) {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		String lset = jedis.lset(key(), index, value);
     		return lset;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public String lindex(int index) {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		String lindex = jedis.lindex(key(), index);
     		return lindex;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Long llen() {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		Long llen = jedis.llen(key());
     		return llen;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Long lrem(int count, String value) {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		Long lrem = jedis.lrem(key(), count, value);
     		return lrem;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public List<String> lrange(int start, int end) {
     	Jedis jedis = null;
+    	Boolean ex =false;
     	try{
     		jedis = getResource();
     		List<String> lrange = jedis.lrange(key(), start, end);
     		return lrange;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     // Redis SortedSet Operations
     public Set<String> zrange(int start, int end) {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		Set<String> zrange = jedis.zrange(key(), start, end);
     		return zrange;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	}  catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
     
     public Set<String> zrangebyscore(double min, double max) {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		Set<String> zrange = jedis.zrangeByScore(key(), min, max);
     		return zrange;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
     
     public Set<String> zrangebyscore(String min, String max) {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		Set<String> zrange = jedis.zrangeByScore(key(), min, max);
     		return zrange;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Long zadd(float score, String member) {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		Long zadd = jedis.zadd(key(), score, member);
     		return zadd;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
     
     public Long zadd(double score, String member) {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		Long zadd = jedis.zadd(key(), score, member);
     		return zadd;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex){
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public void zinterstore(final String dstkey, ZParams params){
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		jedis.zinterstore(dstkey, params, (String[])keys.toArray(new String[0]));
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex){
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Long zrem(String member) {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		Long zrem = jedis.zrem(key(), member);
     		return zrem;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex) {
+    			returnResource(jedis);
+    		}
     	}
     }
 
     public Long zcard() {
     	Jedis jedis = null;
+    	Boolean ex = false;
     	try{
     		jedis = getResource();
     		Long zadd = jedis.zcard(key());
     		return zadd;
-    	}finally{
-    		if (jedis != null) returnResource(jedis);
+    	} catch (Exception e) {
+			e.printStackTrace();
+			ex = true;
+			if (jedis != null) {
+				returnBrokenResource(jedis);
+			}
+			throw new JOhmException(e.getMessage(), JOhmExceptionMeta.GENERIC_EXCEPTION);
+	    } finally{
+    		if (jedis != null && !ex){
+    			returnResource(jedis);
+    		}
     	}
     }
 
@@ -475,6 +795,10 @@ public class Nest<T> {
         jedisPool.returnResource(jedis);
     }
 
+	public void returnBrokenResource(final Jedis jedis) {
+		jedisPool.returnBrokenResource(jedis);
+	}
+	
     public Jedis getResource() {
         Jedis jedis;
         jedis = jedisPool.getResource();
